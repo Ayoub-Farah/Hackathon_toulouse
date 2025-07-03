a17af35ef466a9d84a46339aebf3a15e1b3e22c3
Ayoub Farah <ayoub.farahTS@gmail.com>
2025-07-03 18:35:02 +0200

    Adding the communication protocol for the hackathon

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,35 @@
 /* --------------OWNTECH APIs---------------------------------- */
 #include "SpinAPI.h"
 #include "TaskAPI.h"
+#include "CommunicationAPI.h"
+
+#define MMC_LEAD 0
+#define MMC_SM1 1
+#define MMC_SM2 2
+#define MMC_SM3 3
+#define MMC_SM4 4
+#define MMC_SM5 5
+#define MMC_SM6 6
+
+/**
+ * @brief This function is considering a byte called 'cmd' 
+ *        which can turn on or off signals.
+ *        The signals are identified by their 'id'.
+ *        The value 'val' is used to set the signal:
+ *        - if val is true, the signal is set to 1
+ *        - if val is false, the signal is set to 0
+ */
+#define SET_SIGNAL(cmd, id, val) \
+    do { \
+        if (val) (cmd) |= (1 << (id)); \
+        else (cmd) &= ~(1 << (id)); \
+    } while(0)
+
+/**
+ * @brief This function is to get the turn on/off state of a signal
+ *        identified by its 'id' from a byte called 'cmd'.
+ */
+#define GET_SIGNAL(cmd, id) (((cmd) >> (id)) & 0x01)
 
 /* --------------SETUP FUNCTIONS DECLARATION------------------- */
 
@@ -43,9 +72,62 @@ void loop_critical_task();
 
 /* --------------USER VARIABLES DECLARATIONS------------------- */
 
+/* TODO : Define module_ID depending on the ID of the board */
+uint8_t module_ID = MMC_LEAD;
 
+static uint8_t module_comand; // The command ethe slave needs to apply
+
+/**
+ * This is a structure that defines the frame 
+ * that will be sent and received through the RS485 communication.
+ * command is a byte that contains the state of the signals
+ * Capacitor_Voltage is the voltage of the capacitor
+ * ID is the ID of the module
+ */
+struct MMC_frame {
+	uint8_t command;
+	float32_t Capacitor_Voltage;
+	uint8_t ID;
+}__packed;
+
+typedef MMC_frame MMC_frame_t;
+static MMC_frame_t dataTX_mmc;
+static MMC_frame_t dataRX_mmc;
+
+float32_t MMC_capacitor_voltage[6];
+
+
+uint8_t buffer_tx[6];
+uint8_t buffer_rx[6];
 
 /* --------------SETUP FUNCTIONS------------------------------- */
+
+void reception_function(void)
+{
+	dataRX_mmc = *(MMC_frame_t *) buffer_rx;
+
+	if(Module_ID == MMC_LEAD)
+	{
+		MMC_capacitor_voltage[dataRX_mmc.ID-1] = dataRX_mmc.Capacitor_Voltage;
+	}
+
+	else
+		{
+            if(dataRX_mmc.ID == MMC_LEAD)
+            {
+                module_comand = GET_SIGNAL(dataRX_mmc.command, dataRX_mmc.ID);	
+            }
+			
+			if((dataRX_mmc.ID == module_ID-1))
+			{
+			    dataTX_mmc.ID = module_ID;
+			   // dataTX_mmc.Capacitor_Voltage = MMC_voltage; /* TODO :uncomment when we get the voltage */
+			    memcpy(buffer_tx, &dataTX_mmc, sizeof(dataTX_mmc));
+			   communication.rs485.startTransmission();
+			}
+	
+		}
+}
 
 /**
  * This is the setup routine.
@@ -63,12 +145,16 @@ void setup_routine()
                             task.createBackground(loop_background_task);
 
     /* Uncomment following line if you use the critical task */
-    /* task.createCritical(loop_critical_task, 500); */
+    task.createCritical(loop_critical_task, 100);
 
     /* Finally, start tasks */
     task.startBackground(background_task_number);
     /* Uncomment following line if you use the critical task */
-    /* task.startCritical(); */
+    task.startCritical();
+
+    communication.rs485.configure(buffer_tx, buffer_rx, sizeof(buffer_rx),
+				      reception_function,
+				      SPEED_10M); // custom configuration for RS485
 }
 
 /* --------------LOOP FUNCTIONS-------------------------------- */
@@ -101,7 +187,15 @@ void loop_background_task()
  */
 void loop_critical_task()
 {
+	SET_SIGNAL(data_mmc.command, data_mmc.MMC_SM1, 0);
+	SET_SIGNAL(data_mmc.command, data_mmc.MMC_SM2, 1);
+	SET_SIGNAL(data_mmc.command, data_mmc.MMC_SM3, 1);
+	SET_SIGNAL(data_mmc.command, data_mmc.MMC_SM4, 1);
+	SET_SIGNAL(data_mmc.command, data_mmc.MMC_SM5, 1);
+	SET_SIGNAL(data_mmc.command, data_mmc.MMC_SM6, 1);
 
+    memcpy(buffer_tx, &data_mmc, sizeof(data_mmc));
+    communication.rs485.startTransmission();
 }
 
 /**
