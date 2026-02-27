072017a89f99dbf24c69bd72178f4600992c3d68
Ayoub Farah <ayoub.farahTS@gmail.com>
2026-02-27 13:46:06 +0100

    Commit test de variation duty cycle en bloqué réussi

--- src/main.cpp
+++ src/main.cpp
@@ -226,7 +226,7 @@ static bool send_idle = false;            // Flag to send idle command from mast
 constexpr uint8_t MMC_STATUS_CODE_BITS = 3;
 constexpr uint32_t MMC_STATUS_CODE_MASK = (1UL << MMC_STATUS_CODE_BITS) - 1U;
 constexpr uint32_t MMC_STATUS_UPPER_ARM_MASK = (1UL << MMC_STATUS_CODE_BITS);
-constexpr float32_t DMIN_MIN = 0.0F;
+constexpr float32_t DMIN_MIN = 0.02F;
 constexpr float32_t DMIN_MAX = 0.05F;
 constexpr float32_t DMIN_STEP = 0.0005F;
 constexpr float32_t DMIN_ENCODE_SCALE = 2000.0F; // 1/0.0005
@@ -263,7 +263,7 @@ struct MMC_frame
     } sm_insertion;
     uint16_t capacitor_voltage_raw : 12;
     uint16_t arm_current_raw : 12;
-    uint16_t dmin : 12; // Minimum duty cycle to apply in power mode, encoded on 12 bits to be sent from lead to followers  
+    float32_t dmin; // Minimum duty cycle to apply in power mode, encoded on 12 bits to be sent from lead to followers  
     union
     {
         uint8_t raw;
@@ -690,7 +690,7 @@ void reception_function(void)
             /* retrieving command from lead message*/
             module_comand = static_cast<uint8_t>(
                 mmc_frame_get_sm_inserted(dataRX_mmc, module_ID));
-            dmin = mmc_decode_dmin(dataRX_mmc.dmin); // Update the minimum duty cycle to apply in power mode from the lead message
+            dmin = dataRX_mmc.dmin; // Update the minimum duty cycle to apply in power mode from the lead message
 
             /* retrieving status */
             if (status_code == POWER)
@@ -840,6 +840,7 @@ void loop_communication_task()
         break;
     case 'i':
         printk("idle mode\n");
+        dmin = DMIN_MIN; // Set the minimum duty cycle to apply in power mode to its minimum value when switching to idle mode
         mode = IDLEMODE;
         break;
     case 'p':
@@ -1011,7 +1012,7 @@ void loop_critical_task()
             mmc_frame_set_sm_identifier(dataTX_mmc, module_ID);
             mmc_frame_set_voltage_raw(dataTX_mmc, mmc_encode_voltage(Cap_voltage));
             mmc_frame_set_current_raw(dataTX_mmc, mmc_encode_current(Arm_current));
-            dataTX_mmc.dmin = mmc_encode_dmin(dmin); // Send the minimum duty cycle to apply in power mode to followers
+            dataTX_mmc.dmin = dmin; // Send the minimum duty cycle to apply in power mode to followers
             memcpy(buffer_tx, &dataTX_mmc, sizeof(dataTX_mmc));
 
             communication.rs485.startTransmission();
